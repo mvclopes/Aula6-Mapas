986ff23d35fa4b7e7f5b4fe1874d2066fdc3519b
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-16 19:57:02 -0300

    Configuração inicial do mapa

--- app/src/main/java/com/mvclopes/mapas/MainActivity.kt
+++ app/src/main/java/com/mvclopes/mapas/MainActivity.kt
@@ -2,14 +2,17 @@ package com.mvclopes.mapas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
 import com.mvclopes.mapas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
     }
 }
